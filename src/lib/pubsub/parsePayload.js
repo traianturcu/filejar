168027abf8a9e7677517afa751ad19974c153f88
Traian <traian@turcu.com>
2024-11-16 04:14:48 +0200

    do not skip if SubscribeURL is undefined

--- src/lib/pubsub/parsePayload.js
+++ src/lib/pubsub/parsePayload.js
@@ -1,27 +1,24 @@
 export const parsePayload = async (body) => {
-  console.log("parsing payload", body);
-
   if (!body) {
     return null;
   }
 
   const { Message, Type, SubscribeURL } = body;
 
-  console.log({ Message, Type, SubscribeURL });
-
-  if (!Message || !Type || !SubscribeURL) {
+  if (!Message || !Type) {
     return null;
   }
 
   // check if the message is a subscription confirmation
   if (Type === "SubscriptionConfirmation") {
     console.log("confirming subscription");
-    await fetch(SubscribeURL);
+    if (SubscribeURL) {
+      await fetch(SubscribeURL);
+    }
     return null;
   }
 
   const payload = JSON.parse(Message);
-  console.log("parsed payload", payload);
 
   if (payload.secret !== process.env.SNS_SECRET) {
     return null;
