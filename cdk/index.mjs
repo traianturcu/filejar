61c8817ceebbde292e422eef5652cf6da41e547a
Traian <traian@turcu.com>
2024-11-16 05:01:46 +0200

    use dev not main

--- cdk/index.mjs
+++ cdk/index.mjs
@@ -3,7 +3,10 @@ import { FileJarStack } from "./FileJarStack.mjs";
 
 const cdkapp = new App();
 
-const env = process.env.BRANCH ?? "dev";
+let env = process.env.BRANCH;
+if (!env || env === "main") {
+  env = "dev";
+}
 const app = process.env.APP_NAME;
 
 new FileJarStack(cdkapp, `${app}-${env}`);
